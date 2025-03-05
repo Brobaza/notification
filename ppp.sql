5a4b3163a1ebdc0e46dbe79d5b642e31f05ab6fe
khoebanh <khoeptlc@gmail.com>
2025-03-05 15:25:53 +0700

    Update ppp.sql

--- ppp.sql
+++ ppp.sql
@@ -6,11 +6,14 @@ CREATE TABLE users (
 
 CREATE TABLE notifications (
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    sender_id INT REFERENCES users(id) ON DELETE SET NULL,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE, 
+    parent_id INT REFERENCES notifications(id) ON DELETE SET NULL, 
     title TEXT NOT NULL,
     message TEXT,
     category VARCHAR(50) CHECK (category IN ('communication', 'file_manager', 'project_ui', 'order')),
     status VARCHAR(20) DEFAULT 'unread' CHECK (status IN ('unread', 'read', 'archived')),
+    metadata JSONB, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -23,14 +26,12 @@ CREATE TABLE notification_status (
 );
 
 CREATE TABLE notification_actions (
-    id SERIAL PRIMARY KEY,
-    notification_id INT REFERENCES notifications(id) ON DELETE CASCADE,
+    notification_id INT PRIMARY KEY REFERENCES notifications(id) ON DELETE CASCADE,
     action_name VARCHAR(50) NOT NULL, -- e.g., "Accept", "Decline", "Pay", "Reply", "Download"
     action_url TEXT NOT NULL
 );
 
 CREATE TABLE notification_tags (
-    id SERIAL PRIMARY KEY,
-    notification_id INT REFERENCES notifications(id) ON DELETE CASCADE,
+    notification_id INT PRIMARY KEY REFERENCES notifications(id) ON DELETE CASCADE,
     tag_name VARCHAR(50) NOT NULL
 );
